def93fc263818a405da7ee0ca92d97c8dae66fbd
kellemnegasi <kellemnegasi@gmail.com>
2021-09-13 21:42:11 +0300

    changed the response of the healthcheck endopoint to json

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintln(w,"status:available")
-	fmt.Fprintf(w,"environment: %s\n",app.config.env)
-	fmt.Fprintf(w,"version,%s",version)
+	data:=map[string]string{
+		"status":"available",
+		"environment":app.config.env,
+		"version":version,
+	}
+
+	js,err := json.Marshal(data)
+	if err!=nil{
+		app.logger.Println(err)
+		http.Error(w,"The server encountered a problem and could not process your request",http.StatusInternalServerError)
+		return
+	}
+	js = append(js,'\n')
+	
+	w.Header().Set("content-Type","application/json")
+	w.Write(js)
 }
