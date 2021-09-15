4bdcc672a7932ccb3e1a3021f571da2125d405aa
kellemnegasi <kellemnegasi@gmail.com>
2021-09-15 17:07:09 +0300

    implemented max limit of json size and  disallowed unknown feild and multiple json body

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -40,7 +40,12 @@ func (app *application) writeJSON(w http.ResponseWriter,status int,data interfac
 }
 
 func (app *application) readJSON(w http.ResponseWriter,r *http.Request,dst interface{}) error{
-	err:= json.NewDecoder(r.Body).Decode(dst)
+	maxBytes:= 1_048_576
+	r.Body = http.MaxBytesReader(w,r.Body,int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err:= dec.Decode(dst)
 	if err!=nil{
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -63,6 +68,10 @@ func (app *application) readJSON(w http.ResponseWriter,r *http.Request,dst inter
 		default:
 			return err
 		}
+	}
+	err = dec.Decode(&struct{}{})
+	if err!=io.EOF{
+		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
 }
