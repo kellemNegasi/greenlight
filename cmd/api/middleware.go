79cc1352ffb691590bd44dc39bade8e2efd1593b
kellemnegasi <kellemnegasi@gmail.com>
2021-09-27 22:31:44 +0300

    implemented ip based request rate limiter

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
 
 	"golang.org/x/time/rate"
 )
@@ -20,13 +22,33 @@ func (app *application) recoverPanic(next http.Handler) http.Handler{
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler{
-	limiter:=rate.NewLimiter(2,4)
-	
+	// Declare a mutex and a map to hold the clients' IP addresses and rate limiters.
+	var (
+		mu sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow(){
+		ip,_,err := net.SplitHostPort(r.RemoteAddr)
+		if err!=nil{
+			app.serveErrorResponse(w,r,err)
+			return
+		}
+		// lock this portion of the code from concurency  ------------i.e the code below------
+		mu.Lock()
+		// check if the ip address already exists in the clients map
+		if _,found := clients[ip];!found{
+			clients[ip] = rate.NewLimiter(2,4) // if the ip doesnt exist initialize a new limiter and add it to the map
+		}
+
+		// call the allow method of the limiter for the current ip and 
+		// if not allowed unlocke the mutex and send 429 i.e Too many requests2
+		if !clients[ip].Allow(){
+			mu.Unlock()
 			app.rateLimitExceededResponse(w,r)
 			return
 		}
+		mu.Unlock() // unlock the mutex when the limiter check is done
 		next.ServeHTTP(w,r)
 	})
 } 
