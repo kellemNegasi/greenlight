94303c58356bf46d84c0fb63b9d8c3eb21e01b2f
kellemnegasi <kellemnegasi@gmail.com>
2021-09-28 17:06:29 +0300

    implemented graceful shutdown for  SIGINT and SIGTERM signal calls

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,14 +5,14 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter,r *http.Request){
-	data:=envelope{
+	env:=envelope{
 		"status":"available",
 		"system_info":map[string]string{
 			"environment":app.config.env,
 			"version":version,},
 	}
 
-	err := app.writeJSON(w,http.StatusOK,data,nil)
+	err := app.writeJSON(w,http.StatusOK,env,nil)
 	if err!=nil{
 		app.serveErrorResponse(w,r,err)
 	}
--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -14,10 +19,50 @@ func (app *application) serve() error{
 		IdleTimeout : time.Minute,
 		ReadTimeout: 10*time.Second,
 		WriteTimeout: 30*time.Second,}
+	// create a shutdownerr channel to recieve any error from the graceful shutdown
+	shutdownError := make(chan error)
+	// spin up a background go routine to catch singlas
+	go func(){
+		// create a channel which carries os.Signal. i.e quit channel
+		quit := make(chan os.Signal,1)
+		// listen for incoming SIGINT and SIGTERM signals using signal.notify()
+		// any other signals will not be caught by our channel and should retain theri original behaviour.
+		signal.Notify(quit,syscall.SIGINT,syscall.SIGTERM)
+		// read the signal from the channel.
+		// this code blockes until the signal is recieved
+		s:= <-quit
+		// log the recieving of the signal 
+		app.logger.PrintInfo("shuting down server",map[string]string{
+			"signal":s.String(),
+		})
+		// create a context with 5 seconds timeout
+		ctx,cancel := context.WithTimeout(context.Background(),time.Second*5)
+		defer cancel()
+		// call shutdown() on our server by passing the context
+		shutdownError <- srv.Shutdown(ctx)	
+	}()
 	app.logger.PrintInfo("starting server on",map[string]string{
 		"addr":srv.Addr,
 		"env":app.config.env,
 	})
 
-	return srv.ListenAndServe()
+	err:= srv.ListenAndServe()
+	// calling shutdown causes Listend and server to return the http.ErrServerClosed error.
+	// we are looking for this error. If not just return it to the caller.
+	if !errors.Is(err,http.ErrServerClosed){
+		return err
+	}
+	// otherwise we wait to get the return value from the channel
+	// if the value is an error something must have happend
+	err = <- shutdownError
+	if err !=nil{
+		return err
+	}
+	// now that all the above errors are passed it means 
+	//at this point it is sure that the graceful shutdown has completed successfully
+
+	app.logger.PrintInfo("stopped server ",map[string]string{
+		"addr":srv.Addr,
+	})
+	return nil
 }
