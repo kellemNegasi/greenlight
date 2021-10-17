f470f77bab055e2ba1fb64c9eb5570f3da6c544e
kellemnegasi <kellemnegasi@gmail.com>
2021-10-17 16:50:59 +0300

    updated middleware to respond to prefilight requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -182,13 +182,30 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 func (app *application) enableCORS(next http.Handler) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary","Origin")
+
+		// add the Vary:Access-Control-Request-Method
+		w.Header().Add("Vary","Access-Control-Request-Method")
+
 		origin:= r.Header.Get("Origin")
+
+
 		if origin!="" && len(app.config.cors.trustedOrigins)!=0{
 			// loop through the list of trusted origins checking to see if the request 
 			// origin exactly matches one of them
 			for i:= range app.config.cors.trustedOrigins{
 				if origin==app.config.cors.trustedOrigins[i]{
 					w.Header().Set("Access-Control-Allow-Origin",origin)
+					// check if the request has the HTTP method "OPTIONS" and contains
+					// the "Access-Control-Request-Method" header. if it does then we treat it 
+					// as a prefilight request like so
+
+					if r.Method==http.MethodOptions && r.Header.Get("Access-Control-Request-Method")!=""{
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+
 					
 				}
 			}
